2fc046caa42c82285af7e1cd98eea23043bf5d1f
Ajay Naidu Jami <52778264+AjayNaiduJami@users.noreply.github.com>
2019-07-24 23:02:35 +0530

    Update main.tf
    errors at aws_security_group cidr chnages

--- main.tf
+++ main.tf
@@ -57,13 +57,13 @@ resource "aws_security_group" "BastianSG" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_block = ["0.0.0.0/0"]
   }
   egress {
     from_port = 0
     to_port = 0
     protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_block = ["0.0.0.0/0"]
   }
 
   tags = {
