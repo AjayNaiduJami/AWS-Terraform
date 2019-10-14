9a954835387cf8903f7b70303df7bbc12d4b77c4
Ajay Naidu Jami <52778264+AjayNaiduJami@users.noreply.github.com>
2019-10-14 20:18:35 +0530

    Added LB

--- main.tf
+++ main.tf
@@ -1,57 +1,90 @@
-provider "aws" {
-  region = "ap-south-1"
-}
 
-resource "aws_vpc" "BastianVPC" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "Visitor_Tracking" {
+  cidr_block = "172.32.0.0/16"
     
   tags = {
-    Name = "Bastian"
-    Environment = "Test"
+    Name = "Visitor_Tracking_Sasmodel_Client_DEMO"
+    Environment = "QA"
     Terraform = "Yes"
   }
 }
 
-resource "aws_subnet" "BastianSubnet" {
-  vpc_id     = "${aws_vpc.BastianVPC.id}"
-  cidr_block = "10.0.1.0/24"
+resource "aws_subnet" "Visitor_Tracking_1a" {
+  vpc_id     = "${aws_vpc.Visitor_Tracking.id}"
+  cidr_block = "172.32.0.0/24"
   availability_zone = "ap-south-1a"
   
     tags = {
-    Name = "Bastian"
-    Environment = "Test"
+    Name = "Visitor_Tracking_1a"
+    Environment = "QA"
+    Terraform = "Yes"
+  }
+}
+resource "aws_subnet" "Visitor_Tracking_1b" {
+  vpc_id     = "${aws_vpc.Visitor_Tracking.id}"
+  cidr_block = "172.32.16.0/24"
+  availability_zone = "ap-south-1b"
+  
+    tags = {
+    Name = "Visitor_Tracking_1b"
+    Environment = "QA"
     Terraform = "Yes"
   }
 }
 
-resource "aws_internet_gateway" "BastianIGW" {
-  vpc_id = "${aws_vpc.BastianVPC.id}"
+resource "aws_internet_gateway" "Visitor_Tracking_IGW" {
+  vpc_id = "${aws_vpc.Visitor_Tracking.id}"
+  tags = {
+    Name = "Visitor_Tracking_IGW"
+    Environment = "QA"
+    Terraform = "Yes"
+  }
 }
 
-resource "aws_route_table" "BastianRT" {
-  vpc_id = "${aws_vpc.BastianVPC.id}"
+resource "aws_route_table" "Visitor_Tracking_RT_1a" {
+  vpc_id = "${aws_vpc.Visitor_Tracking.id}"
     
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.BastianIGW.id}"
+    gateway_id = "${aws_internet_gateway.Visitor_Tracking_IGW.id}"
   }
 
   tags = {
-    Name = "RT Bastian"
-    Environment = "Test"
+    Name = "Visitor_Tracking_RT_1a"
+    Environment = "QA"
     Terraform = "Yes"
   }
 }
 
-resource "aws_route_table_association" "RTABastian" {
-  subnet_id      = "${aws_subnet.BastianSubnet.id}"
-  route_table_id = "${aws_route_table.BastianRT.id}"
+resource "aws_route_table_association" "Visitor_Tracking_RTA_1a" {
+  subnet_id      = "${aws_subnet.Visitor_Tracking_1a.id}"
+  route_table_id = "${aws_route_table.Visitor_Tracking_RT_1a.id}" 
 }
 
-resource "aws_security_group" "BastianSG" {
-  name        = "BastianSG"
-  vpc_id = "${aws_vpc.BastianVPC.id}"
-  description = "Allow http, https, SSH"
+resource "aws_route_table" "Visitor_Tracking_RT_1b" {
+  vpc_id = "${aws_vpc.Visitor_Tracking.id}"
+    
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.Visitor_Tracking_IGW.id}"
+  }
+
+  tags = {
+    Name = "Visitor_Tracking_RT_1b"
+    Environment = "QA"
+    Terraform = "Yes"
+  }
+}
+
+resource "aws_route_table_association" "Visitor_Tracking_RTA_1b" {
+  subnet_id      = "${aws_subnet.Visitor_Tracking_1b.id}"
+  route_table_id = "${aws_route_table.Visitor_Tracking_RT_1b.id}" 
+}
+
+resource "aws_security_group" "Visitor_Tracking_Sasmodel_Client_DEMO" {
+  name        = "Visitor_Tracking_Sasmodel_Client_DEMO"
+  vpc_id = "${aws_vpc.Visitor_Tracking.id}"
+  description = "Visitor_Tracking_Sasmodel_Client_DEMO"
 
   ingress {
     from_port = 0
@@ -67,23 +100,74 @@ resource "aws_security_group" "BastianSG" {
   }
 
   tags = {
-    Name = "BastianSG"
-    Environment = "Test"
+    Name = "Visitor_Tracking_Sasmodel_Client_DEMO"
+    Environment = "QA"
     Terraform = "Yes"
   }
 }
 
-resource "aws_instance" "BastianSVR" {
-  ami                    = "ami-009110a2bf8d7dd0a"
+resource "aws_instance" "Visitor_Tracking_Sasmodel_Client_DEMO" {
+  ami                    = "ami-01a762e6f057d4dec"
   instance_type          = "t2.micro"
-  subnet_id              = "${aws_subnet.BastianSubnet.id}"
-  vpc_security_group_ids = ["${aws_security_group.BastianSG.id}"]
+  subnet_id              = "${aws_subnet.Visitor_Tracking_1a.id}"
+  vpc_security_group_ids = ["${aws_security_group.Visitor_Tracking_Sasmodel_Client_DEMO.id}"]
   associate_public_ip_address = "1"
-  key_name = "Terraform"
+  key_name = "Visitortracking-demo-client-test"
   
   tags = {
-    Name = "Bastian SVR"
-    Environment = "Test"
+    Name = "Visitor_Tracking_Sasmodel_Client_DEMO"
+    Environment = "QA"
     Terraform = "Yes"
+  }
+}
+
+resource "aws_lb_target_group" "VisitorTrackingSasmodel" {
+  name        = "VisitorTrackingSasmodel"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "instance"
+  vpc_id      = "${aws_vpc.Visitor_Tracking.id}"
+  tags = {
+    Name = "VisitorTrackingSasmodel"
+    Environment = "QA"
+    Terraform = "Yes" 
+  }
+  health_check {
+    interval =  10
+    port = 80
+    protocol = "HTTP"
+    path = "/"
+    healthy_threshold = 3
+    unhealthy_threshold = 3
+    matcher = 200
+  }
+}
+
+resource "aws_lb_target_group_attachment" "VisitorTrackingSasmodel" {
+  target_group_arn = "${aws_lb_target_group.VisitorTrackingSasmodel.arn}"
+  target_id        = "${aws_instance.Visitor_Tracking_Sasmodel_Client_DEMO.id}"
+  port             = 80
+}
+
+resource "aws_lb" "VisitorTrackingSasmodel" {
+  name               = "VisitorTrackingSasmodel"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = ["${aws_security_group.Visitor_Tracking_Sasmodel_Client_DEMO.id}"]
+  subnets            = ["${aws_subnet.Visitor_Tracking_1a.id}","${aws_subnet.Visitor_Tracking_1b.id}"]
+  tags = {
+    Name = "VisitorTrackingSasmodel"
+    Environment = "QA"
+    Terraform = "Yes" 
+  }
+}
+resource "aws_lb_listener" "VisitorTrackingSasmodel" {
+  load_balancer_arn = "${aws_lb.VisitorTrackingSasmodel.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+  
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.VisitorTrackingSasmodel.arn}"
   }
 }
